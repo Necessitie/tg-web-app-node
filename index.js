501a6d54199a9b15abe870fc061888be5371af4c
Necessities <nik.r060@mail.ru>
2024-07-25 20:13:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,77 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+const webAppUrl = "https://main--grand-frangipane-8cb305.netlify.app";
+const token = '7159722803:AAEaPeR6B_Ba-qwWzQqNGfhzHF_vpcaDKdQ';
+
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message' , async(msg) => {
+
+  const chatId = msg.chat.id;
+  const text = msg.text; 
+  
+  if( text === '/start'){
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+      reply_markup: {
+        keyboard:[
+          [{text: "Заполни форму", web_app: {url: webAppUrl + '/form'}}]
+        ]
+      } 
+    })
+
+      await bot.sendMessage(chatId, 'Ниже появится форма, заполни её', {
+        reply_markup: {
+          inline_keyboard:[
+              [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+          ]
+
+        }
+      })
+  }
+
+    if(msg?.web_app_data?.data) {
+      try{
+        const data = JSON.parse(msg?.web_app_data?.data)
+
+        await bot.sendMessage(chatId,'Спасибо за обратную связь!')
+        await bot.sendMessage(chatId,'Ваша страна:' + data?.country);
+        await bot.sendMessage(chatId,'Ваша улица:' + data?.street);
+
+        setTimeout(async () => {
+          await bot.sendMessage( chatId, 'Всю информацию вы получите в этом чате!')
+          }, 3000)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+})
+app.post('/web-data', async (req,res) => {
+   const {queruId, products, totalPrice} = req.body;
+  try {
+    await bot.answerWebAppQuery(queruId, {
+      type: 'article',
+      id: queruId,
+      title: "Успешная покупка",
+      input_message_content: {message_text: 'Поздравляю с покупкой, вы приобрели товар на сумму' +totalPrice}
+    })
+  }catch(e) {
+    await bot.answerWebAppQuery(queruId, {
+      type: 'article',
+      id: queruId,
+      title: "Не удалось приобрести товар",
+      input_message_content: {message_text: "Не удалось приобрести товар"}
+    })
+  }
+   
+})
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('server started on PORT' + PORT))
+  
+  
